ceb1e3d7e52e0324377e4383ca248174acf8c2d6
Lewin Kelly <heli@odex.cf>
2026-03-20 23:16:01 +0000

    Button formatting, add log out button

--- src/routes/(main)/home/+page.svelte
+++ src/routes/(main)/home/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+	import { enhance } from "$app/forms"
+</script>
+
 <h1>Welcome to logged in</h1>
+
+<form use:enhance method="post" action="/api?/logout">
+	<button class="btn bg-red-500 hover:bg-red-600 active:bg-red-400 px-3 py-1.5 rounded-lg">
+		<b>Log out</b>
+	</button>
+</form>
--- src/routes/(plain)/+page.svelte
+++ src/routes/(plain)/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<a href="/login">log in</a>
+
+<a
+	href="/login"
+	class="btn bg-blue-500 hover:bg-blue-600 active:bg-blue-400 px-3 py-1.5 rounded-lg">
+	Log in with Slack
+</a>
