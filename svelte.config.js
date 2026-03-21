b0c68b592acc14bb49a12c024342a8e3abcbb145
Lewin Kelly <heli@odex.cf>
2026-03-21 01:40:51 +0000

    Improved Svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from "@sveltejs/adapter-auto"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -12,7 +12,24 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+		alias: { $components: "./src/components" },
+
+		experimental: {
+			remoteFunctions: true,
+		},
+	},
+	vitePlugin: { inspector: { toggleKeyCombo: "control-i" } },
+
+	compilerOptions: {
+		// These become defaults in Svelte 6
+		runes: true,
+		modernAst: true,
+
+		// shorter hashes (normal hashes are svelte-{hash}). This doesn't work in development so expect a warning from vite-plugin-svelte, though works fine in production builds.
+		cssHash: ({ hash, filename }) => `s${hash(filename)}`,
+
+		experimental: {
+			async: true,
+		},
 	},
 }
-
-export default config
