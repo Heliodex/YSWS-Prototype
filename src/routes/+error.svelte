870fdae335bfb6be0c7e8d554e8e17d26b0eaf62
Lewin Kelly <heli@odex.cf>
2026-03-21 17:02:07 +0000

    Error page, update grid graphic

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from "$app/state"
+
+	const status = page.status
+</script>
+
+<div class="w-full px-4 py-24 text-center">
+	<h1 class="text-2xl">Error {status}</h1>
+
+	<a href="/">Go home?</a>
+</div>
